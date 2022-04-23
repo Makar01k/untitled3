67ad787b9a9fdb96af06383578e0bd6c53f825db
Макар <Makar.Kochetkov@gmail.com>
2022-04-23 11:52:01 +0300

    Initial commit

---
+++ src/Zoo.java
@@ -0,0 +1,14 @@
+public class Zoo {
+
+    public static void main(String[]args){
+        Person Mixa = new Person();
+        Mixa.say(" Pizda ");
+        System.out.println(Mixa.height);
+        Person Di = new Person();
+        Di.height = 150;
+        System.out.println(Di.height);
+
+
+
+    }
+}
